1ead1afabc669b53e112f43732dcf5975039c42e
Michael Zapf <m.zapf@mztx.de>
2022-10-16 12:25:29 +0200

    esp bridge: improve int parsing / fix bug

--- esp32firmware/yald_esp32_bridge/src/main.cpp
+++ esp32firmware/yald_esp32_bridge/src/main.cpp
@@ -2,9 +2,7 @@
 
 #define CHANNEL_COUNT 32
 
-
 WiFiClient espClient;
-
 uint8_t data[CHANNEL_COUNT];
 
 /*
@@ -65,20 +63,6 @@ void setup() {
     // Initialize data array (should be done automatically, but for sure...)
     memset(data, CHANNEL_COUNT, CHANNEL_COUNT * sizeof(uint8_t));
 
-    /*
-    server.on("/healthz", []() {
-        server.send(200, "text/plain", "OK");
-    });
-
-    server.on("/version", []() {
-        server.send(200, "text/plain", MZTX_FIRMWARE_VERSION);
-    });
-
-    server.on("/data", HTTP_GET, handleGetData);
-    server.on("/data", HTTP_POST, handlePostData);
-    server.on("/", handleRootPath);
-    */
-
    initUpdateServer();
 
     mqttInit();
@@ -94,95 +78,20 @@ void setup() {
 
 inline void SendBreak()
 {
-    //s.end();
-
-/*
-    pinMode(17, OUTPUT);
-    pinMatrixOutAttach(17, 0, false, false);
-    digitalWrite(17, LOW);
-
-    //delay(100);
-    delayMicroseconds(573);
-    //delayMicroseconds(1024);
-*/
-/*
-    s.begin(300, SERIAL_8N1);       // open 300bps
-    byte x[8];
-    memset(x, 0x00, 8);
-    s.write(x, 8);    // 500ms null(0) send => effect Send BREAK
-    s.end();
-*/
-
-    //s.end();
-    //s.begin(19200, SERIAL_8N1);     // reopen 19200bps
-
-/*
-    Serial2.end();
-    pinMatrixOutDetach(17, false, false);
-    pinMode(17, OUTPUT);
-    pinMatrixOutAttach(17, 0, false, false);
-    digitalWrite(17, LOW);
-
-    delayMicroseconds(1);
-    //delay(20);
-    Serial2.begin(19200, SERIAL_8N1);
-
-*/
-
     Serial2.updateBaudRate(9600);
     uint8_t buf[] = {0};
     Serial2.write(buf, sizeof(buf));
     Serial2.flush();
     Serial2.updateBaudRate(38400);
-
-    //Serial2.end();
-
-/*
-    pinMode(17, OUTPUT);
-    pinMatrixOutAttach(17, 0, false, false);
-    digitalWrite(17, LOW);
-
-    delayMicroseconds(573);
-    pinMatrixOutAttach(17, UART_RXD_IDX(2), false, false);
-    //Serial2.begin(19200, SERIAL_8N1);
-*/
-
-/*
-    uartDetachTx(&_uart_bus_array[2]);
-    pinMode(17, OUTPUT);
-    digitalWrite(17, LOW);
-    delayMicroseconds(573);
-    uartAttachTx(&_uart_bus_array[2], 17, false);
-*/
 }
 
 void loop() {
-    // @TODO output repeatedly the current values in separate task / core
-    //const uint8_t nullbytes[] = {0, 0, 0, 0, 0, 0, 0, 0};
-    //MZTX_DEBUG_PRINTLN("loop");
-
     // break
     SendBreak();
-
-/*
-    for (int i = 0; i < CHANNEL_COUNT; ++i) {
-        MZTX_DEBUG_PRINT(data[i]);
-        MZTX_DEBUG_PRINT(',');
-    }
-    MZTX_DEBUG_PRINTLN();
-*/
 
     //Serial2.write(data, 32);
     Serial2.write(data, CHANNEL_COUNT);
     Serial2.flush();
-    /*
-
-    //delay(50);
-
-    //data[2] += 1;
-
-    //server.handleClient();
-    */
 
     loopUpdateServer();
 
@@ -192,52 +101,6 @@ void loop() {
 
     mqttLoop();
 }
-/*
-void handleRootPath() {
-    server.send(200, "text/plain", "See /data , /version , /healthz");
-}
-
-void handleGetData() {
-    const int capacity = JSON_OBJECT_SIZE(CHANNEL_COUNT*4);
-    StaticJsonDocument<capacity> doc;
-
-    for (uint8_t i = 0; i < CHANNEL_COUNT; i++) {
-        char key[5];
-        sprintf(key, "%d", i);
-        doc[key] = data[i];
-    }
-
-    char output[CHANNEL_COUNT*16];
-    serializeJson(doc, output);
-    server.send(200, "text/plain", output);
-}
-
-void handlePostData() {
-    if (server.args() < 1) {
-        server.send(400, "text/plain", "POST data missing");
-        return;
-    }
-
-    const int capacity = JSON_OBJECT_SIZE(CHANNEL_COUNT*4);
-    StaticJsonDocument<capacity> doc;
-    DeserializationError err = deserializeJson(doc, server.arg(0));
-
-    JsonObject object = doc.as<JsonObject>();
-
-    if (err) {
-        server.send(400, "text/plain", err.c_str());
-        return;
-    }
-
-    for (JsonObject::iterator it=object.begin(); it!=object.end(); ++it) {
-        uint8_t key = (uint8_t) atoi(it->key().c_str());
-        uint8_t value = (uint8_t) it->value().as<int>();
-        data[key] = (value & 0xFF);
-    }
-
-    server.send(200, "text/plain", "OK");
-}
-*/
 
 void mqttCallback(char* topic, byte* payload, unsigned int length)
 {
@@ -257,11 +120,13 @@ void mqttCallback(char* topic, byte* payload, unsigned int length)
         data[channel] = (uint8_t) (doc["g"]);
         data[channel+1] = (uint8_t) (doc["b"]);
     } else if (1 == sscanf(topic, MQTT_TOPIC_FORMAT_SET_CHANNEL, &channel)) {
-        uint8_t s[32];
-        strncpy((char*) s, (const char*) payload, min(length, (unsigned int) 32));
-        uint8_t value = 0;
-        value = atoi((const char *)s);
+        // Add missing terminating null byte to payload
+        payload[length] = '\0';
+
+        int valueInt = atoi((const char *) payload);
+        uint8_t value = valueInt & 0xFF;
         data[channel-1] = value;
+
         MZTX_DEBUG_PRINTLN("Channel line");
         MZTX_DEBUG_PRINTLN(channel);
         MZTX_DEBUG_PRINTLN(value);
